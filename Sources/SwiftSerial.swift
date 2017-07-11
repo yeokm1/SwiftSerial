44e11a259c2fe3625a912e851f7250780ef76dba
Yuri <ysoft@mail.ru>
2017-07-11 23:29:23 +0300

    bugfix

--- Sources/SwiftSerial.swift
+++ Sources/SwiftSerial.swift
@@ -362,118 +362,6 @@ public class SerialPort {
         tcsetattr(fileDescriptor, TCSANOW, &settings)
     }
     
-    
-    public func setSettings(customBaud: UInt32,
-                            minimumBytesToRead: Int,
-                            timeout: Int = 0, /* 0 means wait indefinitely */
-        parityType: ParityType = .none,
-        sendTwoStopBits: Bool = false, /* 1 stop bit is the default */
-        dataBitsSize: DataBitsSize = .bits8,
-        useHardwareFlowControl: Bool = false,
-        useSoftwareFlowControl: Bool = false,
-        processOutput: Bool = false) {
-        
-        guard let fileDescriptor = fileDescriptor else {
-            return
-        }
-        
-        
-        // Set up the control structure
-        var settings = termios()
-        
-        // Get options structure for the port
-        tcgetattr(fileDescriptor, &settings)
-        
-        // Enable parity (even/odd) if needed
-        settings.c_cflag |= parityType.parityValue
-        
-        // Set stop bit flag
-        if sendTwoStopBits {
-            settings.c_cflag |= tcflag_t(CSTOPB)
-        } else {
-            settings.c_cflag &= ~tcflag_t(CSTOPB)
-        }
-        
-        // Set data bits size flag
-        settings.c_cflag &= ~tcflag_t(CSIZE)
-        settings.c_cflag |= dataBitsSize.flagValue
-        
-        
-        // Set baud rate
-        // Set baud rates
-        cfsetispeed(&settings, speed_t(customBaud))
-        cfsetospeed(&settings, speed_t(customBaud))
-
-        
-        // Set hardware flow control flag
-        #if os(Linux)
-            if useHardwareFlowControl {
-                settings.c_cflag |= tcflag_t(CRTSCTS)
-            } else {
-                settings.c_cflag &= ~tcflag_t(CRTSCTS)
-            }
-        #elseif os(OSX)
-            if useHardwareFlowControl {
-                settings.c_cflag |= tcflag_t(CRTS_IFLOW)
-                settings.c_cflag |= tcflag_t(CCTS_OFLOW)
-            } else {
-                settings.c_cflag &= ~tcflag_t(CRTS_IFLOW)
-                settings.c_cflag &= ~tcflag_t(CCTS_OFLOW)
-            }
-        #endif
-        
-        // Set software flow control flags
-        let softwareFlowControlFlags = tcflag_t(IXON | IXOFF | IXANY)
-        if useSoftwareFlowControl {
-            settings.c_iflag |= softwareFlowControlFlags
-        } else {
-            settings.c_iflag &= ~softwareFlowControlFlags
-        }
-        
-        // Turn on the receiver of the serial port, and ignore modem control lines
-        settings.c_cflag |= tcflag_t(CREAD | CLOCAL)
-        
-        // Turn off canonical mode
-        settings.c_lflag &= ~tcflag_t(ICANON | ECHO | ECHOE | ISIG)
-        
-        // Set output processing flag
-        if processOutput {
-            settings.c_oflag |= tcflag_t(OPOST)
-        } else {
-            settings.c_oflag &= ~tcflag_t(OPOST)
-        }
-        
-        //Special characters
-        //We do this as c_cc is a C-fixed array which is imported as a tuple in Swift.
-        //To avoid hardcoding the VMIN or VTIME value to access the tuple value, we use the typealias instead
-        #if os(Linux)
-            typealias specialCharactersTuple = (VINTR: cc_t, VQUIT: cc_t, VERASE: cc_t, VKILL: cc_t, VEOF: cc_t, VTIME: cc_t, VMIN: cc_t, VSWTC: cc_t, VSTART: cc_t, VSTOP: cc_t, VSUSP: cc_t, VEOL: cc_t, VREPRINT: cc_t, VDISCARD: cc_t, VWERASE: cc_t, VLNEXT: cc_t, VEOL2: cc_t, spare1: cc_t, spare2: cc_t, spare3: cc_t, spare4: cc_t, spare5: cc_t, spare6: cc_t, spare7: cc_t, spare8: cc_t, spare9: cc_t, spare10: cc_t, spare11: cc_t, spare12: cc_t, spare13: cc_t, spare14: cc_t, spare15: cc_t)
-            var specialCharacters: specialCharactersTuple = (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0) // NCCS = 32
-        #elseif os(OSX)
-            typealias specialCharactersTuple = (VEOF: cc_t, VEOL: cc_t, VEOL2: cc_t, VERASE: cc_t, VWERASE: cc_t, VKILL: cc_t, VREPRINT: cc_t, spare1: cc_t, VINTR: cc_t, VQUIT: cc_t, VSUSP: cc_t, VDSUSP: cc_t, VSTART: cc_t, VSTOP: cc_t, VLNEXT: cc_t, VDISCARD: cc_t, VMIN: cc_t, VTIME: cc_t, VSTATUS: cc_t, spare: cc_t)
-            var specialCharacters: specialCharactersTuple = (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0) // NCCS = 20
-        #endif
-        
-        specialCharacters.VMIN = cc_t(minimumBytesToRead)
-        specialCharacters.VTIME = cc_t(timeout)
-        
-        //if not set we reciving 0x0A where must be 0x0D value
-        settings.c_iflag &= ~tcflag_t(INLCR)
-        settings.c_iflag &= ~tcflag_t(ICRNL)
-        
-        settings.c_cc = specialCharacters
-        
-        // Commit settings
-        var result = tcsetattr(fileDescriptor, TCSANOW, &settings)
-        
-        var baudRate = Int(customBaud)
-        result = ioctl(fileDescriptor, 0x80045402, &baudRate)
-        if result != 0 {
-            print("set customBaud fail \(result)")
-        }
-        
-        
-    }
 
     public func closePort() {
         if let fileDescriptor = fileDescriptor {
@@ -646,7 +534,7 @@ extension SerialPort {
                     data.append(buffer[0])
                     if data.count >= packetLength {
                         if data[data.count - packetLength - 1] == startByte {
-                            let data_result: Array<UInt8> = Array(data[(data.count - packetLength - 1) ... data.count])
+                            let data_result: Array<UInt8> = Array(data[(data.count - packetLength - 1) ... data.count - 1])
                             return data_result
                         }
                     }
